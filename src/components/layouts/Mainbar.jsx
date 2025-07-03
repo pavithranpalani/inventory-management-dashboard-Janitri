0843037d92923b87cbfcda3ca0c7e16f26534920
PAVITHRAN <pavithran95palani@gmail.com>
2025-07-03 15:33:38 +0530

    proceeding with satatic mainbar

--- src/components/layouts/Mainbar.jsx
+++ src/components/layouts/Mainbar.jsx
@@ -1,8 +1,21 @@
+import Stack from '@mui/material/Stack'
 import React from 'react'
 
 const Mainbar = () => {
   return (
-    <div>Mainbar</div>
+    <Stack spacing={4} direction="column" sx={{background:"#3e4444", color:"#fff",}}>
+    <Stack spacing={2} direction="column" >
+
+        <div>
+            
+        </div>
+        <div>
+            <div>About</div>
+            <div>Contact Us</div>
+        </div>
+    </Stack>
+    <Stack spacing={2} direction="column"></Stack>
+    </Stack>
   )
 }
 
