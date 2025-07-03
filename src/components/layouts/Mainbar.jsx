e31eebf45afb993bf45c79ddff2eed38ba7bac50
PAVITHRAN <pavithran95palani@gmail.com>
2025-07-03 17:33:13 +0530

    render patchname dynamically in mainbar

--- src/components/layouts/Mainbar.jsx
+++ src/components/layouts/Mainbar.jsx
@@ -1,42 +1,70 @@
-import Stack from '@mui/material/Stack'
-import React from 'react'
-import ThemeToggleProvider from '../../ThemeToggleProvider.jsx'
-import ThemeToggleSwitch from '../../ThemeToggleSwitch.jsx'
-import { Route, Routes } from "react-router-dom"
-import Dashboard from '../modules/Dashboard.jsx'
-import InsTraining from '../modules/InsTraining.jsx'
-import ServiceVlog from '../modules/ServiceVlog.jsx'
-import AmcCmcTracker from '../modules/AmcCmcTracker.jsx'
-import AlertPhoto from '../modules/AlertPhoto.jsx'
+import Stack from "@mui/material/Stack";
+import React from "react";
+import ThemeToggleProvider from "../../ThemeToggleProvider.jsx";
+import ThemeToggleSwitch from "../../ThemeToggleSwitch.jsx";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Dashboard from "../modules/Dashboard.jsx";
+import InsTraining from "../modules/InsTraining.jsx";
+import ServiceVlog from "../modules/ServiceVlog.jsx";
+import AmcCmcTracker from "../modules/AmcCmcTracker.jsx";
+import AlertPhoto from "../modules/AlertPhoto.jsx";
 
 const Mainbar = () => {
+  const location = useLocation();
   return (
     <ThemeToggleProvider>
-    <Stack spacing={4} direction="column" sx={{background:"#3e4444", color:"#fff", fontSize:"1.2rem", height:"100vh", width:"80vw",}}>
-    <Stack spacing={2} direction="row" sx={{display:'flex', flexDirection:'row', justifyContent:'space-between', alignItems:'center', padding:"15px",}}>
-
-        <Stack>
-            dashboard   
-        </Stack>
-        <Stack spacing={2} direction="row" display='flex' flexDirection='row' justifyContent='center' alignItems='center'>
+      <Stack
+        spacing={4}
+        direction="column"
+        sx={{
+          background: "#3e4444",
+          color: "#fff",
+          fontSize: "1.2rem",
+          height: "100vh",
+          width: "80vw",
+        }}
+      >
+        <Stack
+          spacing={2}
+          direction="row"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "15px",
+          }}
+        >
+          <Stack>
+            {location.pathname === "/"
+              ? "DASHBOARD"
+              : location.pathname.split("/")[1].toUpperCase()}
+          </Stack>
+          <Stack
+            spacing={2}
+            direction="row"
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+          >
             <div>About</div>
             <div>Contact Us</div>
             <ThemeToggleSwitch />
-
+          </Stack>
         </Stack>
-    </Stack>
-    <Stack spacing={2} direction="column">
-        <Routes>
-        <Route path="/" element={<Dashboard/>}/>
-        <Route path="/instraining" element={<InsTraining/>}/>
-        <Route path="/service" element={<ServiceVlog/>}/>
-        <Route path="/amc" element={<AmcCmcTracker/>}/>
-        <Route path="/photo" element={<AlertPhoto/>}/>
-      </Routes>
-    </Stack>
-    </Stack>
+        <Stack spacing={2} direction="column">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/instraining" element={<InsTraining />} />
+            <Route path="/service" element={<ServiceVlog />} />
+            <Route path="/amc" element={<AmcCmcTracker />} />
+            <Route path="/photo" element={<AlertPhoto />} />
+          </Routes>
+        </Stack>
+      </Stack>
     </ThemeToggleProvider>
-  )
-}
+  );
+};
 
-export default Mainbar
+export default Mainbar;
